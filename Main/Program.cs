3f3344abff7bf2aa3b809f0706f4bed9e3bbc62a
Natalie Wong <wongn341@gmail.com>
2020-03-04 15:29:26 -0500

    Contains a menu and serialization methods

--- Main/Program.cs
+++ Main/Program.cs
@@ -2,11 +2,11 @@
 // File: Program.cs
 //
 // Purpose: Contains the class definition for Program.
-//          Program will read the Worker and Shift from 
-//          a JSON and XML file, write Worker and Shift
-//          to a JSON and XML file, write both Worker
-//          and Shift to an Excel file, and display
-//          Worker and Shift data on the screen.      
+//          Program will read the Department from 
+//          a JSON and XML file, write Department
+//          to a JSON and XML file, write Department
+//          to an Excel file, display Department
+//          data on the screen, and find a worker.
 //
 // Written By: Natalie Wong
 //
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 
-namespace Main2
+namespace Main
 {
     public class Program
     {
@@ -33,27 +33,21 @@ namespace Main2
         /// <returns></returns>
         private static void Main(string[] args)
         {
-            var worker = new Worker();
-            var shift = new Shift();
+            var department = new Department();
             int choice;
 
             do
             {
-                Console.WriteLine("Payroll Menu");
-                Console.WriteLine("------------");
-                Console.WriteLine("1 - Read Worker from JSON file");
-                Console.WriteLine("2 - Read Worker from XML file");
-                Console.WriteLine("3 - Write Worker to JSON file");
-                Console.WriteLine("4 - Write Worker to XML file");
-                Console.WriteLine("5 - Write Worker to Excel file");
-                Console.WriteLine("6 - Display Worker data on screen");
-                Console.WriteLine("7 - Read Shift from JSON file");
-                Console.WriteLine("8 - Read Shift from XML file");
-                Console.WriteLine("9 - Write Shift to JSON file");
-                Console.WriteLine("10 - Write Shift to XML file");
-                Console.WriteLine("11 - Write Shift to Excel file");
-                Console.WriteLine("12 - Display Shift data on screen");
-                Console.WriteLine("13 - Exit");
+                Console.WriteLine("Department Menu");
+                Console.WriteLine("---------------");
+                Console.WriteLine("1 - Read department from JSON file");
+                Console.WriteLine("2 - Read department from XML file");
+                Console.WriteLine("3 - Write department to JSON file");
+                Console.WriteLine("4 - Write department to XML file");
+                Console.WriteLine("5 - Write department to Excel file");
+                Console.WriteLine("6 - Display all department data on screen");
+                Console.WriteLine("7 - Find worker");
+                Console.WriteLine("8 - Exit");
                 Console.Write("Enter Choice: ");
 
                 int userInput = Convert.ToInt32(Console.ReadLine());
@@ -66,19 +60,7 @@ namespace Main2
                         try
                         {
                             string fileName = UserEnteringFileName();
-                            worker = DeserializeWorkerJSON(fileName);
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine($"Error: {exception.Message}");
-                        }
-                        break;
-
-                    case 2:
-                        try
-                        {
-                            string fileName = UserEnteringFileName();
-                            worker = DeserializeWorkerXML(fileName);
+                            department = DeserializeDepartmentJSON(fileName);
                         }
                         catch (Exception exception)
                         {
@@ -90,7 +72,7 @@ namespace Main2
                         try
                         {
                             string fileName = UserEnteringFileName();
-                            SerializeWorkerJSON(fileName, worker);
+                            SerializeDepartmentJSON(fileName, department);
                         }
                         catch (Exception exception)
                         {
@@ -98,38 +80,15 @@ namespace Main2
                         }
                         break;
 
-                    case 4:
-                        try
-                        {
-                            string fileName = UserEnteringFileName();
-                            SerializeWorkerXML(fileName, worker);
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine($"Error: {exception.Message}");
-                        }
-                        break;
-
-                    case 5:
-                        try
-                        {
-                            string fileName = UserEnteringFileName();
-                            WriteWorkerToExcel(fileName, worker);
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine($"Error: {exception.Message}");
-                        }
-                        break;
                     case 6:
-                        Console.WriteLine(worker.ToString());
+                        Console.WriteLine(department.ToString());
                         break;
 
                     case 7:
                         try
                         {
-                            string fileName = UserEnteringFileName();
-                            shift = DeserializeShiftJSON(fileName);
+                            Console.WriteLine("Enter worker id: ");
+                            int userEntered = Convert.ToInt32(Console.ReadLine());
                         }
                         catch (Exception exception)
                         {
@@ -137,58 +96,8 @@ namespace Main2
                         }
                         break;
 
-                    case 8:
-                        try
-                        {
-                            string fileName = UserEnteringFileName();
-                            shift = DeserializeShiftXML(fileName);
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine($"Error: {exception.Message}");
-                        }
-                        break;
-
-                    case 9:
-                        try
-                        {
-                            string fileName = UserEnteringFileName();
-                            SerializeShiftJSON(fileName, shift);      
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine($"Error: {exception.Message}");
-                        }
-                        break;
-
-                    case 10:
-                        try
-                        {
-                            string fileName = UserEnteringFileName();
-                            SerializeShiftXML(fileName, shift);
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine($"Error: {exception.Message}");
-                        }
-                        break;
-
-                    case 11:
-                        try
-                        {
-                            string fileName = UserEnteringFileName();
-                            WriteShiftToExcel(fileName, shift);
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine($"Error: {exception.Message}");
-                        }
-                        break;
-                    case 12:
-                        Console.WriteLine(shift.ToString());
-                        break;
                 }
-            } while (choice != 13);
+            } while (choice != 8);
         }
 
         /// <summary>
@@ -204,262 +113,34 @@ namespace Main2
             return fileName;
         }
 
-        #region Worker Methods
         /// <summary>
-        /// Saves Worker data in JSON file format <see cref="Worker"/>
+        /// Reads Department from JSON file <see cref="Department"/>
         /// </summary>
         /// <returns></returns>
-        public static void SerializeWorkerJSON(string fileName, Worker worker)
+        public static Department DeserializeDepartmentJSON(string fileName)
         {
-            var writer = new FileStream(fileName, FileMode.Create, FileAccess.Write);
-            var dataContractJsonSerializer = new DataContractJsonSerializer(typeof(Worker));
-
-            dataContractJsonSerializer.WriteObject(writer, worker);
-            writer.Close();
-        }
-
-        /// <summary>
-        /// Reads Worker from JSON file <see cref="Worker"/>
-        /// </summary>
-        /// <returns></returns>
-        public static Worker DeserializeWorkerJSON(string fileName)
-        {
-            Worker worker;
+            Department department;
 
             var reader = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            var inputSerializer = new DataContractJsonSerializer(typeof(Worker));
+            var inputSerializer = new DataContractJsonSerializer(typeof(Department));
 
-            worker = (Worker)inputSerializer.ReadObject(reader);
+            department = (Department)inputSerializer.ReadObject(reader);
             reader.Close();
 
-            return worker;
+            return department;
         }
 
         /// <summary>
-        /// Reads Worker from XML file <see cref="Worker"/>
+        /// Saves Department data in JSON file format <see cref="Department"/>
         /// </summary>
         /// <returns></returns>
-        public static Worker DeserializeWorkerXML(string fileName)
-        {
-            Worker worker;
-
-            var reader = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            var inputSerializer = new DataContractSerializer(typeof(Worker));
-
-            worker = (Worker)inputSerializer.ReadObject(reader);
-            reader.Close();
-
-            return worker;
-        }
-
-        /// <summary>
-        /// Saves Worker data in XML file format <see cref="Worker"/>
-        /// </summary>
-        /// <returns></returns>
-        public static void SerializeWorkerXML(string fileName, Worker worker)
+        public static void SerializeDepartmentJSON(string fileName, Department department)
         {
             var writer = new FileStream(fileName, FileMode.Create, FileAccess.Write);
-            var serXml = new DataContractSerializer(typeof(Worker)); // For serializing to XML
+            var dataContractJsonSerializer = new DataContractJsonSerializer(typeof(Department));
 
-            serXml.WriteObject(writer, worker);
+            dataContractJsonSerializer.WriteObject(writer, department);
             writer.Close();
-        }
-
-        /// <summary>
-        /// Writes Worker to Excel file <see cref="Worker"/>
-        /// </summary>
-        /// <returns></returns>
-        public static void WriteWorkerToExcel(string fileName, Worker worker)
-        {
-            Application excelApp;
-            Workbooks excelWorkBooks;
-            _Workbook excelWorkBook;
-            _Worksheet excelWorkSheet;
-
-            // Start Excel and get Application object.
-            excelApp = new Application
-            {
-                Visible = false
-            };
-
-            // Get a new workbook and worksheet
-            excelWorkBooks = excelApp.Workbooks;
-            excelWorkBook = excelWorkBooks.Add();
-            excelWorkSheet = (_Worksheet)excelWorkBook.ActiveSheet;
-
-            excelWorkSheet.Cells[1, 1] = "Name";
-            excelWorkSheet.Cells[1, 2] = "Id";
-            excelWorkSheet.Cells[1, 3] = "Pay Rate";
-
-            excelWorkSheet.Cells[2, 1] = worker.Name;
-            excelWorkSheet.Cells[2, 2] = worker.Id;
-            excelWorkSheet.Cells[2, 3] = worker.PayRate;
-
-            Range excelRange = excelWorkSheet.Range["A1", "C1"];
-            excelRange.Font.Bold = true;
-            excelRange.Font.Underline = true;
-
-            excelWorkBook.SaveAs(fileName);
-            excelWorkBook.Close();
-            excelApp.Quit();
-
-            // Need to release COM objects.These are hidden resources that get allocated
-            if (excelWorkSheet != null)
-            {
-                Marshal.ReleaseComObject(excelWorkSheet);
-            }
-
-            if (excelWorkBook != null)
-            {
-                Marshal.ReleaseComObject(excelWorkBook);
-            }
-
-            if (excelWorkBooks != null)
-            {
-                Marshal.ReleaseComObject(excelWorkBooks);
-            }
-
-            if (excelApp != null)
-            {
-                Marshal.ReleaseComObject(excelApp);
-            }
-
-            if (excelRange != null)
-            {
-                Marshal.ReleaseComObject(excelRange);
-            }
-        }
-        #endregion
-
-        #region Shift Methods
-        /// <summary>
-        /// Saves Shift data in JSON file format <see cref="Shift"/>
-        /// </summary>
-        /// <returns></returns>
-        public static void SerializeShiftJSON(string fileName, Shift shift)                                                             
-        {
-            var writer = new FileStream(fileName, FileMode.Create, FileAccess.Write);
-            var dataContractJsonSerializer = new DataContractJsonSerializer(typeof(Shift));
-
-            dataContractJsonSerializer.WriteObject(writer, shift);
-            writer.Close();
-        }
-
-        /// <summary>
-        /// Reads Shift from JSON file <see cref="Shift"/>
-        /// </summary>
-        /// <returns></returns>
-        public static Shift DeserializeShiftJSON(string fileName)
-        {
-            Shift shift;
-
-            var reader = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            var inputSerializer = new DataContractJsonSerializer(typeof(Shift));
-
-            shift = (Shift)inputSerializer.ReadObject(reader);
-            reader.Close();
-
-            return shift;
-        }
-
-        /// <summary>
-        /// Reads Shift from XML file <see cref="Shift"/>
-        /// </summary>
-        /// <returns></returns>
-        public static Shift DeserializeShiftXML(string fileName)
-        {
-            Shift shift;
-
-            var reader = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            var inputSerializer = new DataContractSerializer(typeof(Shift));
-
-            shift = (Shift)inputSerializer.ReadObject(reader);
-            reader.Close();
-
-            return shift;
-        }
-
-        /// <summary>
-        /// Saves Shift data in XML file format <see cref="Shift"/>
-        /// </summary>
-        /// <returns></returns>
-        public static void SerializeShiftXML(string fileName, Shift shift)
-        {
-            var writer = new FileStream(fileName, FileMode.Create, FileAccess.Write);
-            var serXml = new DataContractSerializer(typeof(Shift)); // For serializing to XML
-
-            serXml.WriteObject(writer, shift);
-            writer.Close();
-        }
-
-        /// <summary>
-        /// Writes Shift to Excel file <see cref="Shift"/>
-        /// </summary>
-        /// <returns></returns>
-        public static void WriteShiftToExcel(string fileName, Shift shift)
-        {
-            int month = shift.Date.Month;
-            int day = shift.Date.Day;
-            int year = shift.Date.Year;
-
-            Application excelApp;
-            Workbooks excelWorkBooks;
-            _Workbook excelWorkBook;
-            _Worksheet excelWorkSheet;
-
-            // Start Excel and get Application object.
-            excelApp = new Application
-            {
-                Visible = false
-            };
-
-            // Get a new workbook and worksheet
-            excelWorkBooks = excelApp.Workbooks;
-            excelWorkBook = excelWorkBooks.Add();
-            excelWorkSheet = (_Worksheet)excelWorkBook.ActiveSheet;
-
-            excelWorkSheet.Cells[1, 1] = "Worker Id";
-            excelWorkSheet.Cells[1, 2] = "Hours Worked";
-            excelWorkSheet.Cells[1, 3] = "Date";
-
-            excelWorkSheet.Cells[2, 1] = shift.WorkerID;
-            excelWorkSheet.Cells[2, 2] = shift.HoursWorked;
-            excelWorkSheet.Cells[2, 3] = month + "/" + day + "/" + year;
-
-            Range excelRange = excelWorkSheet.Range["A1", "C1"];
-            excelRange.Font.Bold = true;
-            excelRange.Font.Underline = true;
-
-            excelWorkBook.SaveAs(fileName);
-            excelWorkBook.Close();
-            excelApp.Quit();
-
-            // Need to release COM objects.These are hidden resources that get allocated
-            if (excelWorkSheet != null)
-            {
-                Marshal.ReleaseComObject(excelWorkSheet);
-            }
-
-            if (excelWorkBook != null)
-            {
-                Marshal.ReleaseComObject(excelWorkBook);
-            }
-
-            if (excelWorkBooks != null)
-            {
-                Marshal.ReleaseComObject(excelWorkBooks);
-            }
-
-            if (excelApp != null)
-            {
-                Marshal.ReleaseComObject(excelApp);
-            }
-
-            if (excelRange != null)
-            {
-                Marshal.ReleaseComObject(excelRange);
-            }
         }
     }
-    #endregion
 }
