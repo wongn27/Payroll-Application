d4b9122e7035360046afaff136c65191074957e5
Natalie Wong <wongn341@gmail.com>
2020-03-09 13:18:00 -0400

    Added a method and case statement to generate the Excel file

--- Main/Program.cs
+++ Main/Program.cs
@@ -35,7 +35,7 @@ namespace Main
         {
             var department = new Department();
             int choice;
-
+            
             do
             {
                 Console.WriteLine("Department Menu");
@@ -73,6 +73,18 @@ namespace Main
                         {
                             string fileName = UserEnteringFileName();
                             SerializeDepartmentJSON(fileName, department);
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine($"Error: {exception.Message}");
+                        }
+                        break;
+
+                    case 5:
+                        try
+                        {
+                            string fileName = UserEnteringFileName();
+                            WriteDepartmentToExcel(fileName, department);
                         }
                         catch (Exception exception)
                         {
@@ -142,5 +154,96 @@ namespace Main
             dataContractJsonSerializer.WriteObject(writer, department);
             writer.Close();
         }
+
+
+
+
+
+        /// <summary>
+        /// Writes Worker to Excel file <see cref="Department"/>
+        /// </summary>
+        /// <returns></returns>
+        public static void WriteDepartmentToExcel(string fileName, Department department)
+        {
+            Application excelApp;
+            Workbooks excelWorkBooks;
+            _Workbook excelWorkBook;
+            _Worksheet excelWorkSheet;
+
+            //Start Excel and get Application object.
+            excelApp = new Application
+            {
+                Visible = false
+            };
+
+            //Get a new workbook and worksheet
+            excelWorkBooks = excelApp.Workbooks;
+            excelWorkBook = excelWorkBooks.Add();
+            excelWorkSheet = (_Worksheet)excelWorkBook.ActiveSheet;
+
+            // nested for loop for inputing data in to cells
+            excelWorkSheet.Cells[1, 1] = "Department Name";
+            excelWorkSheet.Cells[1, 2] = "Technical Support";
+            excelWorkSheet.Cells[3, 1] = "Workers";
+            excelWorkSheet.Cells[3, 6] = "Shifts";
+            excelWorkSheet.Cells[5, 1] = "Name";
+            excelWorkSheet.Cells[5, 2] = "Id";
+            excelWorkSheet.Cells[5, 3] = "Pay Rate";
+            excelWorkSheet.Cells[5, 6] = "Worker Id";
+            excelWorkSheet.Cells[5, 7] = "Hours Worked";
+            excelWorkSheet.Cells[5, 8] = "Date";
+
+            int workerRow = 6;
+            foreach (Worker worker in department.Workers)
+            {
+                excelWorkSheet.Cells[workerRow, 1] = worker.Name;
+                excelWorkSheet.Cells[workerRow, 2] = worker.Id;
+                excelWorkSheet.Cells[workerRow, 3] = worker.PayRate;
+                ++workerRow;
+            }
+            int shiftRow = 6;
+            foreach (Shift shift in department.Shifts)
+            {
+                excelWorkSheet.Cells[shiftRow, 6] = shift.WorkerID;
+                excelWorkSheet.Cells[shiftRow, 7] = shift.HoursWorked;
+                excelWorkSheet.Cells[shiftRow, 8] = shift.Date;
+                ++shiftRow;
+            }
+
+            ////Range excelRange = excelWorkSheet.Range["A1"];
+            //excelRange.Font.Bold = true;
+            //excelRange.Font.Underline = true;
+
+            excelWorkBook.SaveAs(fileName);
+            excelWorkBook.Close();
+            excelApp.Quit();
+
+            // Need to release COM objects.These are hidden resources that get allocated
+            if (excelWorkSheet != null)
+            {
+                Marshal.ReleaseComObject(excelWorkSheet);
+            }
+
+            if (excelWorkBook != null)
+            {
+                Marshal.ReleaseComObject(excelWorkBook);
+            }
+
+            if (excelWorkBooks != null)
+            {
+                Marshal.ReleaseComObject(excelWorkBooks);
+            }
+
+            if (excelApp != null)
+            {
+                Marshal.ReleaseComObject(excelApp);
+            }
+
+            //if (excelRange != null)
+            //{
+            //    Marshal.ReleaseComObject(excelRange);
+            //}
+        }
+
     }
 }
